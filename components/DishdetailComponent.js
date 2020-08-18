768228001229d8e4fd1d8513225da729e21a8983
Javier Hevia <javierhc22@gmail.com>
2020-08-18 14:33:52 -0600

    Gestos Parte 2

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -15,15 +15,17 @@ const mapStateToProps = state => {
     }
 }
 
-
-
 const mapDispatchToProps = dispatch => ({
     postFavorite: (dishId) => dispatch(postFavorite(dishId)),
     postComment: (dishId, rating, author, comment) => dispatch(postComment(dishId, rating, author, comment))
 });
 
 function RenderDish(props) {
-    
+
+    const dish = props.dish;
+
+    handleViewRef = ref => this.view = ref;
+
     const recognizeDrag = ({ moveX, moveY, dx, dy }) => {
         if (dx < -200)
             return true;
@@ -35,6 +37,7 @@ function RenderDish(props) {
         onStartShouldSetPanResponder: (e, gestureState) => {
             return true;
         },
+        onPanResponderGrant: () => { this.view.rubberBand(1000).then(endState => console.log(endState.finished ? 'finished' : 'cancelled')); },
         onPanResponderEnd: (e, gestureState) => {
             console.log("pan responder end", gestureState);
             if (recognizeDrag(gestureState))
@@ -51,11 +54,11 @@ function RenderDish(props) {
             return true;
         }
     })
-    const dish = props.dish;
+
 
     if (dish != null) {
         return (
-            <Animatable.View animation="fadeInDown" duration={2000} delay={1000} {...panResponder.panHandlers}>
+            <Animatable.View animation="fadeInDown" duration={2000} delay={1000} ref={this.handleViewRef} {...panResponder.panHandlers}>
                 <Card
                     featuredTitle={dish.name}
                     featuredTitle={dish.name}
